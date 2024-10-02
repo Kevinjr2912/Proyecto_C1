bc5d5c7948534a7c3b44983b1689514da9ab21d0
Kevin <moralesperezkevin7@gmail.com>
2024-10-02 17:50:11 -0600

    add: Iniciando componente add-recipe-page

---
+++ recipe_project_front/src/app/add-recipe-page/add-recipe-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-recipe-page',
+  templateUrl: './add-recipe-page.component.html',
+  styleUrl: './add-recipe-page.component.css'
+})
+export class AddRecipePageComponent {
+
+}
