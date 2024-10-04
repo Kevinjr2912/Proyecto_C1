73488574dbbb52de882e0840deb58610cf6b6236
Kevin <moralesperezkevin7@gmail.com>
2024-10-04 00:01:50 -0600

    View2 ready, solo faltan detallitos

---
+++ recipe_project_front/src/app/see-published-recipes-page/see-published-recipes-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-see-published-recipes-page',
+  templateUrl: './see-published-recipes-page.component.html',
+  styleUrl: './see-published-recipes-page.component.css'
+})
+export class SeePublishedRecipesPageComponent {
+
+}
